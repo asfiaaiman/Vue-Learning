21ad8f76b92c9de7d42d0017b681f396f9204b2e
aimanasfia <aiman.asfia@gmail.com>
2023-08-22 13:21:05 +0500

    I have learnt declarative rendering

--- src/App.vue
+++ src/App.vue
@@ -1,80 +1,17 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-// Calling Parent in views here
-// import Parent from './views/parent.vue'
+import { reactive, ref } from 'vue'
+
+// component logic
+// declare some reactive state here.
+  const age = reactive({
+    myAge: 28
+  })
+  const message = ref('Hey There, Asfia')
 </script>
 
-
 <template>
-
-
-<!-- We are using Parent Component here -->
-  <!-- <Parent/> -->
-
-  <RouterView />
+  <h1>{{ message.split('').reverse().join('') }}</h1>
+  <p style="margin: 20px;">
+    My age is is : {{ age.myAge }}
+  </p>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
